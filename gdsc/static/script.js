c4f96516ea764e39dae1047dcaf22a11a46a4953
Drishtti Narwal <drishtti.narwal@gmail.com>
2024-03-07 18:39:53 +0530

    homepage animation, events  done

--- gdsc/static/script.js
+++ gdsc/static/script.js
@@ -1,15 +1,32 @@
-const spans = document.querySelectorAll('.word span');
+gsap.to('.text0', {
+    x: '20%', rotate: 10, duration: 0.85, scrollTrigger: {
+        trigger: '.text0',
+        scrub: true,
+        start: 'top 20%'
+    }
+})
+gsap.to('.text2', {
+    x: '20%', rotate: 10, duration: 0.85, scrollTrigger: {
+        trigger: '.text2',
+        scrub: true,
+        start: 'top 40%'
+    }
+})
+gsap.to('.text1', {
+    x: '-20%', rotate: -10, duration: 0.85, scrollTrigger: {
+        trigger: '.text1',
+        scrub: true,
+        start: 'top 30%'
+    }
+})
+gsap.to('.text3', {
+    x: '-20%', rotate: -10, duration: 0.85, scrollTrigger: {
+        trigger: '.text3',
+        scrub: true,
+        start: 'top 40%'
+    }
+})
+document.querySelector('#back').addEventListener('click', () => {
+    gsap.to(window, { duration: 1, scrollTo: 'body' });
 
-spans.forEach((span, idx) => {
-    span.addEventListener('click', (e) => {
-        e.target.classList.add('active');
-    });
-    span.addEventListener('animationend', (e) => {
-        e.target.classList.remove('active');
-    });
-
-    // Initial animation
-    setTimeout(() => {
-        span.classList.add('active');
-    }, 750 * (idx + 1))
-});
+})
